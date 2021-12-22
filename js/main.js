3766476af59914a47bd6d36bcde8c08be76a4d56
TigDang <gizetdinov.m.f@gmail.com>
2021-12-22 23:23:37 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+//Make the DIV element draggagle:
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function onDragStart(event) {
+  event
+    .dataTransfer
+    .setData('text/plain', event.target.id);
+}
+
+function onDragOver(event) {
+  event.preventDefault();
+}
+
+function removeDropzone(dropzone){
+  dropzone.remove();
+}
+
+function onDrop(event) {
+  const id = event.dataTransfer.getData('text');
+  const draggableElement = document.getElementById(id);
+  const dropzone = event.target;
+  console.log(library);
+  console.log(draggableElement);
+
+  console.log(library.has(draggableElement.innerText));
+
+  if (library.get(draggableElement.innerText)===dropzone.innerText
+    || library.get(dropzone.innerText)===draggableElement.innerText) {
+    score++;
+    countScore();
+    dropzone.style.backgroundColor="#7109AA";
+  }
+  else {
+    dropzone.style.backgroundColor='#FF0000';
+  }
+
+  draggableElement.remove();
+  event.dataTransfer.clearData();
+  setTimeout(removeDropzone, 2500, dropzone)
+  if (score===library.size-1){
+    clearInterval(createNewPairs)
+    document.body.style.backgroundColor='#FF80C8';
+    document.getElementById('gameOverLabel').hidden=false;
+  }
+}
+
+idCounter = 0;
+
+function makePair(){
+  let ans = document.getElementById('ansEthalon').cloneNode();
+  let qst = document.getElementById('questEthalon').cloneNode();
+
+  ans.id=idCounter+'ANS';
+  qst.id=idCounter+'QST';
+  idCounter++;
+
+  ans.hidden=false;
+  qst.hidden=false;
+
+  ans.style.top=getRandomArbitrary(0, 500)+'px';
+  ans.style.left=getRandomArbitrary(0, 1200)+'px';
+
+  qst.style.top=getRandomArbitrary(0, 500)+'px';
+  qst.style.left=getRandomArbitrary(0, 1200)+'px';
+
+  ans.innerText = stack.keys().next().value;
+  qst.innerText = stack.get(stack.keys().next().value);
+  stack.delete(stack.keys().next().value);
+
+  if (ans.innerText!=='undefined'){
+    document.getElementById('gameField').appendChild(ans);
+    document.getElementById('gameField').appendChild(qst);
+  }
+}
+
+function countScore(){
+  document.getElementById('score').innerText=score;
+
+}
+
+countScore();
+
+createNewPairs = setInterval(makePair, 4000);
+
